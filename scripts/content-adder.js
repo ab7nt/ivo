e5b74517db2a7f8728a6dfad6106053120917070
ab7nt <85429584+ab7nt@users.noreply.github.com>
2022-06-18 21:03:00 +0300

    add author.html and postScript

---
+++ scripts/content-adder.js
@@ -0,0 +1,46 @@
+(() => {
+  const formTitle = document.querySelector('#formTitle');
+  const formText = document.querySelector('#formText');
+  const postForm = document.querySelector('#postForm');
+  const postDate = `${new Date().getDate()}.${new Date().getMonth() + 1}.${new Date().getFullYear()}`
+
+  const posts = [
+    {
+      id: 1,
+      date: '12.12.2022',
+      title: `Презентация книги "ИВО - Начало".`,
+      text: 'Раздача автографов нахаляву.'
+    },
+    {
+      id: 2,
+      date: '11.11.2022',
+      title: 'Квартирник у Гали.',
+      text: 'Уютная компания, приятные люди, ламповая атмосфера. Исполнялись как новые, так и старые песни.'
+    },
+    {
+      id: 3,
+      date: '10.10.2022',
+      title: 'Релиз нового альбома.',
+      text: 'Рождённый в муках. Слушать на всех платформах.'
+    }
+  ];
+  
+  function createPost() {
+    const post = {};
+
+    post.id = posts.length + 1;
+    post.date = postDate;
+    post.title = formTitle.value;
+    post.text = formText.value;
+
+    posts.push(post);
+    localStorage.setItem('posts', JSON.stringify(posts))
+  }
+
+  postForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    createPost();
+    
+    e.target.reset();
+  })
+})();
---
+++ scripts/main.js
@@ -0,0 +1,27 @@
+(() => {
+  const blogList = document.querySelector('#blogList');
+  postsFromLocalStorage = JSON.parse(localStorage.getItem('posts'));
+
+  postsFromLocalStorage.forEach((el) => {
+    blogList.innerHTML += `
+    <li class="blog-list-item border rounded">
+      <div class="blog-post">
+        <div class="date-wrapper">
+          <div class="post-date">
+            ${el.date}
+          </div>
+        </div>
+        <div class="blog-content">
+          <h5 class="post-title mb-2">
+            ${el.title}
+          </h5>
+          <div class="partition border-bottom"></div>
+          <p class="blog-descr pt-1">
+            ${el.text}
+          </p>
+        </div>
+      </div>
+    </li>
+    `
+  })
+})();
